014545f7cf0b0e7892008c999da2f55738f3497b
张蕾 <1lei.zhang@zhaopin.com.cn>
2020-04-05 18:48:41 +0800

    feat:解析ES6和React JSX

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { HelloWorld } from './helloWorld'
+
+document.write(HelloWorld())
---
+++ src/search.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const Search = () => {
+  return <div>Search Text</div>
+}
+
+ReactDom.render(<Search />, document.getElementById('root'))
