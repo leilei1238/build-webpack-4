189af490452ed52ff696e8bdcdd64edec609b996
张蕾 <1lei.zhang@zhaopin.com.cn>
2020-04-06 16:01:42 +0800

    feat:多页面打包

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,52 @@
 'use strict'
 
 const path = require('path')
+const fs = require('fs')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const glob = require('glob')
 
+const setMPA = () => {
+  const entry = {}
+  const htmlWebpackPlugin = []
+
+  const entryFiles = glob.sync(path.join(__dirname, 'src/*/index.js'))
+  Object.keys(entryFiles).map((index) => {
+    const entryFile = entryFiles[index]
+    //"/Users/zhanglei/project/webpack-pro/src/index/index.js"
+    const match = entryFile.match(/src\/(.+)\/index\.js/)
+    const pageName = match && match[1]
+    entry[pageName] = entryFile
+
+    htmlWebpackPlugin.push(
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, `src/${pageName}/index.html`),
+        filename: `${pageName}.html`,
+        chunks: [pageName],
+        inject: true,
+        minify: {
+          html5: true,
+          collapseWhitespace: true,
+          preserveLineBreaks: false,
+          minifyCSS: true,
+          minifyJS: true,
+          removeComments: false,
+        },
+      })
+    )
+  })
+
+  return {
+    entry,
+    htmlWebpackPlugin,
+  }
+}
+
+const { entry, htmlWebpackPlugin } = setMPA()
 module.exports = {
-  entry: {
-    index: './src/index/index.js',
-    search: './src/search/index.js',
-  },
+  entry,
   output: {
     filename: '[name]_[chunkhash:8].js',
     path: path.join(__dirname, 'dist'),
@@ -103,34 +139,6 @@ module.exports = {
       assetNameRegExp: /\.css$/g,
       cssProcessor: require('cssnano'),
     }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/search/index.html'),
-      filename: 'search.html',
-      chunks: ['search'],
-      inject: true,
-      minify: {
-        html5: true,
-        collapseWhitespace: true,
-        preserveLineBreaks: false,
-        minifyCSS: true,
-        minifyJS: true,
-        removeComments: false,
-      },
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/index/index.html'),
-      filename: 'index.html',
-      chunks: ['index'],
-      inject: true,
-      minify: {
-        html5: true,
-        collapseWhitespace: true,
-        preserveLineBreaks: false,
-        minifyCSS: true,
-        minifyJS: true,
-        removeComments: false,
-      },
-    }),
     new CleanWebpackPlugin(),
-  ],
+  ].concat(htmlWebpackPlugin),
 }
