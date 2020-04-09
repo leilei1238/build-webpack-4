4bf4b300b5906beb0861ea17849a5813b03e7869
张蕾 <1lei.zhang@zhaopin.com.cn>
2020-04-09 14:33:09 +0800

    feat:builder travis ci

--- test/unit/webpack.base.test.js
+++ test/unit/webpack.base.test.js
@@ -4,7 +4,6 @@ const projectRoot = process.cwd()
 
 describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base')
-  console.log(baseConfig)
   it('entry', () => {
     assert.equal(baseConfig.entry.index, `${projectRoot}/src/index/index.js`)
     assert.equal(baseConfig.entry.search, `${projectRoot}/src/search/index.js`)
